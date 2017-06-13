1782bb13be7eeaf90d1d5b66a84dbabf39471c53
ARTBLND\ArtBlnd <junryoungju@gmail.com>
2017-06-13 11:32:03 +0900

    fixed & added comments.

--- src/main/java/Base/IBitcoinSiteApi.java
+++ src/main/java/Base/IBitcoinSiteApi.java
@@ -8,7 +8,7 @@ public interface IBitcoinSiteApi
     public void Refresh(EnumCoinTypes type) throws ExceptionUnsupportedCoinType;
     public void Refresh();
     
-    // Get max price in 24 hours
+    // Get max price in 24 hours.
     // 24시간 이내의 최고가를 얻어냅니다.
     public int getMaxPrice(EnumCoinTypes type) throws ExceptionUnsupportedPriceType;
 
@@ -21,11 +21,11 @@ public interface IBitcoinSiteApi
     public int getAvgPrice(EnumCoinTypes type) throws ExceptionUnsupportedPriceType;
 
     // Get first price in 24 hours.
-    // 24시간에서 첫번째 가격을 가져옵니다.
+    // 24시간 이내의 첫번째 가격을 가져옵니다.
     public int getFirstPrice(EnumCoinTypes type) throws ExceptionUnsupportedPriceType;
 
     // Get last price in 24 hours.
-    // 24시간에서 마지막 가격을 가져옵니다.
+    // 24시간 이내의 마지막 가격을 가져옵니다.
     public int getLastPrice(EnumCoinTypes type) throws ExceptionUnsupportedPriceType;
 
     // Get cheapest sell price from site(that you can buy).
--- src/main/java/BitcoinApi.java
+++ src/main/java/BitcoinApi.java
@@ -22,38 +22,57 @@ public class BitcoinApi
         SiteApis[EnumSiteTypes.OkCoin.ordinal()] = new OkCoinApi();
     }
 
+    // Get max price in 24 hours from target site.
+    // 24시간 이내의 최고가를 사이트로부터 얻어냅니다.
     public int getMaxPrice(EnumSiteTypes site, EnumCoinTypes type) throws ExceptionUnsupportedPriceType
     {
         return SiteApis[site.ordinal()].getMaxPrice(type);
     }
 
+    // Get min price in 24 hours from target site.
+    // 24시간 이내의 최소가를 사이트로부터 얻어냅니다.
     public int getMinPrice(EnumSiteTypes site, EnumCoinTypes type) throws ExceptionUnsupportedPriceType
     {
         return SiteApis[site.ordinal()].getMinPrice(type);
     }
 
+    // Get avg price in 24 hours from target site.
+    // 24시간 이내의 평균가를 사이트로부터 얻어냅니다.
     public int getAvgPrice(EnumSiteTypes site, EnumCoinTypes type) throws ExceptionUnsupportedPriceType
     {
         return SiteApis[site.ordinal()].getAvgPrice(type);
     }
 
+    // Get first price in 24 hours from target site.
+    // 24시간 이내의 첫번째 가격을 사이트로부터 가져옵니다.
     public int getFirstPrice(EnumSiteTypes site, EnumCoinTypes type) throws ExceptionUnsupportedPriceType
     {
         return SiteApis[site.ordinal()].getFirstPrice(type);
     }
+
+    // Get last price in 24 hours from target site.
+    // 24시간 이내의 마지막 가격을 사이트로부터 가져옵니다.
     public int getLastPrice(EnumSiteTypes site, EnumCoinTypes type) throws ExceptionUnsupportedPriceType
     {
         return SiteApis[site.ordinal()].getLastPrice(type);
     }
+
+    // Get cheapest sell price from target site(that you can buy).
+    // 가장 싼 가격의 비트코인가를 살수있는(사이트에서 파는.) 가격을 사이트로부터 가져옵니다.
     public int getSellPrice(EnumSiteTypes site, EnumCoinTypes type) throws ExceptionUnsupportedPriceType
     {
         return SiteApis[site.ordinal()].getSellPrice(type);
     }
+
+    //  Get cheapest buy price from target site(that you can sell).
+    // 가장 싼 비싼의 비트코인가를 팔수있는(사이트에서 사는.) 가격을 사이트로부터 가져옵니다.
     public int getBuyPrice(EnumSiteTypes site, EnumCoinTypes type) throws ExceptionUnsupportedPriceType
     {
         return SiteApis[site.ordinal()].getBuyPrice(type);
     }
-
+    
+    // Refresh all site infomation.
+    // 모든 사이트 정보를 갱신합니다.
     public void Refresh() throws Exception
     {
         for(int indexSite = 0; indexSite < SiteTypeAvailable; ++indexSite)
@@ -61,10 +80,14 @@ public class BitcoinApi
             SiteApis[indexSite].Refresh();
         }
     }
+
+    // Refresh target site's infomation.
     public void Refresh(EnumSiteTypes site) throws Exception
     {
         SiteApis[site.ordinal()].Refresh();
     }
+
+    // Refresh target coin type's infomation in all sites.
     public void Refresh(EnumCoinTypes type) throws Exception
     {
         for(int indexSite = 0; indexSite < SiteTypeAvailable; ++indexSite)
@@ -72,6 +95,8 @@ public class BitcoinApi
             SiteApis[indexSite].Refresh(type);
         }
     }
+
+    // Refresh infomation target coin type in target site.
     public void Refresh(EnumSiteTypes site, EnumCoinTypes type) throws Exception
     {
         SiteApis[site.ordinal()].Refresh(type);
